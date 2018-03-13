b838468c9a82faa1e0796af7add92c569f71494f
doganekici <doganekici26@gmail.com>
2018-03-13 16:02:12 +0300

    Update android

--- instabug-Android.js
+++ instabug-Android.js
@@ -1,3 +1,46 @@
 function InstaBug() {}
 
+InstaBug.ios = {};
+InstaBug.android = {};
+
+InstaBug.InvocationEvent = {}
+
+InstaBug.build = function(token,invocationEvent){}
+
+InstaBug.invoke = function(){}
+
+InstaBug.InvocationMode = {}
+
+InstaBug.invokeWithInvocationMode = function(invocationMode){}
+
+InstaBug.dismiss = function(){}
+
+InstaBug.setShakingThreshold = function(shakingThreshold){}
+
+InstaBug.Theme = {}
+
+InstaBug.setTheme = function(theme){}
+
+InstaBug.setPrimaryColor = function(color){}
+
+InstaBug.showIntroMessage = function(){}
+
+InstaBug.setPromptOptionsEnabled = function(chat,bug,feedback){}
+
+InstaBug.identifyUser = function(name,email){}
+
+InstaBug.setUserAttribute = function(key,value){}
+
+InstaBug.removeUserAttribute = function(key){}
+
+InstaBug.getAllUserAttributes = function(){}
+
+InstaBug.getUserAttribute = function(key){}
+
+InstaBug.logoutUser = function(){}
+
+InstaBug.Locale = {}
+
+InstaBug.setLocale = function(locale){}
+
 module.exports = InstaBug;
